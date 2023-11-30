7aff8363a8c968d9c8bae1ef863584c76cdc8db8
ashenye <luren145@gmail.com>
2023-11-30 22:54:59 +0800

    x

--- ast/src/lib.rs
+++ ast/src/lib.rs
@@ -1,9 +1,20 @@
+use std::fmt::Display;
+
 pub type Loc = usize;
 
 pub type Sourced<T = ()> = (Loc, T);
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Module<'a>(Vec<Sourced<Unit<'a>>>);
+pub struct Module<'a>(pub Vec<Sourced<Unit<'a>>>);
+
+impl<'a> Display for Module<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for unit in &self.0 {
+            write!(f, "{}\n", unit.1)?;
+        }
+        Ok(())
+    }
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Unit<'a> {
@@ -14,20 +25,44 @@ pub enum Unit<'a> {
     FuncDef(Sourced<FuncDef<'a>>),
 }
 
+impl<'a> Display for Unit<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Unit::ConstDecl(unit) => write!(f, "{}", unit.1),
+            Unit::GlobalVarDecl(unit) => write!(f, "{}", unit.1),
+            Unit::ExternalFuncDecl(unit) => write!(f, "{};\n", unit.1),
+            Unit::FuncDecl(unit) => write!(f, "{};\n", unit.1),
+            Unit::FuncDef(unit) => write!(f, "{}", unit.1),
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum PrimType {
     Void,
     Bool,
-    // Char,
     Int,
     Float,
 }
 
+impl Display for PrimType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                PrimType::Void => "void",
+                PrimType::Bool => "bool",
+                PrimType::Int => "int",
+                PrimType::Float => "float",
+            }
+        )
+    }
+}
+
 pub type BoolLiteral = bool;
-// pub type CharLiteral = u8;
 pub type IntLiteral = i32;
 pub type FloatLiteral = f32;
-// pub type StringLiteral = String;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Literal {
@@ -40,20 +75,15 @@ pub enum Literal {
     List(Vec<Sourced<Literal>>),
 }
 
-impl ToString for Literal {
-    fn to_string(&self) -> String {
+impl Display for Literal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            // AstLiteral_::Null => "null".to_owned(),
-            Literal::Bool(val) => match val {
-                true => "true".to_owned(),
-                false => "false".to_owned(),
-            },
-            // AstLiteral_::Char(ch) => format!("'{}'", (*ch as char).escape_default()),
-            Literal::Int(num) => num.to_string(),
-            Literal::Float(num) => format!("{num:.1}"),
-            // AstLiteral_::String(s) => format!("\"{}\"", s.escape_default()),
+            Literal::Bool(val) => write!(f, "{}", val),
+            Literal::Int(num) => write!(f, "{}", num),
+            Literal::Float(num) => write!(f, "{num:.1}"),
             Literal::List(list) => {
-                format!(
+                write!(
+                    f,
                     "{{{}}}",
                     list.iter()
                         .map(|item| item.1.to_string())
@@ -70,7 +100,7 @@ pub enum Type {
     Prim(Sourced<PrimType>),
     // Ptr(Box<AstType>),
     // 多维数组, 对于参数中不声明第一维那么数组的首个元素用usize::MAX占位
-    Array(Sourced<PrimType>, Sourced<Vec<usize>>),
+    Array(Sourced<PrimType>, Sourced<Vec<i32>>),
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -80,11 +110,62 @@ pub struct ConstDecl<'a> {
     pub init: Sourced<Literal>,
 }
 
+impl<'a> Display for ConstDecl<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.ty.1 {
+            Type::Prim((_, ty)) => write!(f, "const {} {} = {};\n", ty, self.ident.1, self.init.1),
+            Type::Array((_, ty), (_, dim)) => {
+                let dim = dim.iter().fold(String::new(), |mut acc, item| {
+                    acc.push_str(&format!("[{}]", item));
+                    acc
+                });
+                write!(
+                    f,
+                    "const {} {}{} = {};\n",
+                    ty, self.ident.1, dim, self.init.1
+                )
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct GlobalVarDecl<'a> {
     pub ty: Sourced<Type>,
     pub ident: Sourced<&'a str>,
     pub init: Option<Sourced<Literal>>,
+}
+
+impl<'a> Display for GlobalVarDecl<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.ty.1 {
+            Type::Prim((_, ty)) => {
+                if let Some(init) = &self.init {
+                    write!(f, "{} {} = {};\n", ty, self.ident.1, init.1)
+                } else {
+                    write!(f, "{} {};\n", ty, self.ident.1)
+                }
+            }
+            Type::Array((_, ty), (_, dim)) => {
+                let dim = dim.iter().fold(String::new(), |mut acc, item| {
+                    acc.push_str(&format!("[{}]", item));
+                    acc
+                });
+                if let Some(init) = &self.init {
+                    write!(
+                        f,
+                        "{} {}{} = {};\n",
+                        ty,
+                        self.ident.1,
+                        dim,
+                        init.1.to_string()
+                    )
+                } else {
+                    write!(f, "{} {}{};\n", ty, self.ident.1, dim)
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -94,11 +175,57 @@ pub struct VarDecl<'a> {
     pub init: Option<Sourced<Expr<'a>>>,
 }
 
+impl<'a> Display for VarDecl<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.ty.1 {
+            Type::Prim((_, ty)) => {
+                if let Some(init) = &self.init {
+                    write!(f, "{} {} = {};\n", ty, self.ident.1, init.1)
+                } else {
+                    write!(f, "{} {};\n", ty, self.ident.1)
+                }
+            }
+            Type::Array((_, ty), (_, dim)) => {
+                let dim = dim.iter().fold(String::new(), |mut acc, item| {
+                    acc.push_str(&format!("[{}]", item));
+                    acc
+                });
+                if let Some(init) = &self.init {
+                    write!(
+                        f,
+                        "{} {}{} = {};\n",
+                        ty,
+                        self.ident.1,
+                        dim,
+                        init.1.to_string()
+                    )
+                } else {
+                    write!(f, "{} {}{};\n", ty, self.ident.1, dim)
+                }
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct FuncParam<'a> {
     pub ty: Sourced<Type>,
     pub ident: Sourced<&'a str>,
-    pub dimension: Vec<usize>,
+}
+
+impl<'a> Display for FuncParam<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.ty.1 {
+            Type::Prim((_, ty)) => write!(f, "{} {}", ty, self.ident.1),
+            Type::Array((_, ty), (_, dim)) => {
+                let dim = dim.iter().fold(String::new(), |mut acc, item| {
+                    acc.push_str(&format!("[{}]", item));
+                    acc
+                });
+                write!(f, "{} {}{}", ty, self.ident.1, dim)
+            }
+        }
+    }
 }
 pub type FuncParams<'a> = Vec<Sourced<FuncParam<'a>>>;
 pub type FuncArgs<'a> = Vec<Sourced<Expr<'a>>>;
@@ -108,17 +235,40 @@ pub struct FuncProto<'a> {
     pub ident: Sourced<&'a str>,
     pub params: Sourced<FuncParams<'a>>,
 }
+
+impl<'a> Display for FuncProto<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let params = self
+            .params
+            .1
+            .iter()
+            .map(|param| param.1.to_string())
+            .collect::<Vec<String>>()
+            .join(", ");
+        write!(f, "{} {}({})", self.ty.1, self.ident.1, params)
+    }
+}
 #[derive(Debug, Clone, PartialEq)]
 pub struct FuncDef<'a> {
     pub proto: Sourced<FuncProto<'a>>,
     pub body: Sourced<Block<'a>>,
 }
 
+impl<'a> Display for FuncDef<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {{\n", self.proto.1)?;
+        for stmt in &self.body.1 {
+            write!(f, "{}", stmt.1)?;
+        }
+        write!(f, "}}\n")
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Stmt<'a> {
     ConstDecl(Sourced<ConstDecl<'a>>),
     VarDecl(Sourced<VarDecl<'a>>),
-    Assign(Sourced<AssginStmt<'a>>),
+    Assign(Sourced<AssignStmt<'a>>),
     If(Sourced<IfStmt<'a>>),
     While(Sourced<WhileStmt<'a>>),
     Break(Sourced),
@@ -128,10 +278,45 @@ pub enum Stmt<'a> {
     Block(Sourced<Block<'a>>),
 }
 
+impl<'a> Display for Stmt<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Stmt::ConstDecl(stmt) => write!(f, "{}", stmt.1),
+            Stmt::VarDecl(stmt) => write!(f, "{}", stmt.1),
+            Stmt::Assign(stmt) => write!(f, "{}", stmt.1),
+            Stmt::If(stmt) => write!(f, "{}", stmt.1),
+            Stmt::While(stmt) => write!(f, "{}", stmt.1),
+            Stmt::Break(_) => write!(f, "break;\n"),
+            Stmt::Continue(_) => write!(f, "continue;\n"),
+            Stmt::Return(stmt) => {
+                if let Some(expr) = &stmt.1 {
+                    write!(f, "return {};\n", expr.1)
+                } else {
+                    write!(f, "return;\n")
+                }
+            }
+            Stmt::Expr(stmt) => write!(f, "{};\n", stmt.1),
+            Stmt::Block(stmt) => {
+                write!(f, "{{\n")?;
+                for stmt in &stmt.1 {
+                    write!(f, "{}", stmt.1)?;
+                }
+                write!(f, "}}\n")
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
-pub struct AssginStmt<'a> {
-    lhs: Sourced<LValue<'a>>,
-    rhs: Sourced<Expr<'a>>,
+pub struct AssignStmt<'a> {
+    pub lhs: Sourced<LValue<'a>>,
+    pub rhs: Sourced<Expr<'a>>,
+}
+
+impl<'a> Display for AssignStmt<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} = {};\n", self.lhs.1, self.rhs.1)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -145,10 +330,54 @@ pub enum IfStmt<'a> {
     ),
 }
 
+impl<'a> Display for IfStmt<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            IfStmt::If(cond, body) => {
+                write!(f, "if ({}) {{\n", cond.1)?;
+                for stmt in &body.1 {
+                    write!(f, "{}", stmt.1)?;
+                }
+                write!(f, "}}\n")
+            }
+            IfStmt::IfElse(cond, body, else_body) => {
+                write!(f, "if ({}) {{\n", cond.1)?;
+                for stmt in &body.1 {
+                    write!(f, "{}", stmt.1)?;
+                }
+                write!(f, "}} else {{\n")?;
+                for stmt in &else_body.1 {
+                    write!(f, "{}", stmt.1)?;
+                }
+                write!(f, "}}\n")
+            }
+            IfStmt::IfElseIf(cond, body, else_if) => {
+                write!(f, "if ({}) {{\n", cond.1)?;
+                for stmt in &body.1 {
+                    write!(f, "{}", stmt.1)?;
+                }
+                write!(f, "}} else ")?;
+                write!(f, "{}", else_if.1)?;
+                Ok(())
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct WhileStmt<'a> {
     pub cond: Sourced<Expr<'a>>,
     pub body: Sourced<Block<'a>>,
+}
+
+impl<'a> Display for WhileStmt<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "while ({}) {{\n", self.cond.1)?;
+        for stmt in &self.body.1 {
+            write!(f, "{}", stmt.1)?;
+        }
+        write!(f, "}}\n")
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -158,12 +387,12 @@ pub enum Expr<'a> {
     Binary(Sourced<Binary<'a>>),
 }
 
-impl ToString for Expr<'_> {
-    fn to_string(&self) -> String {
+impl<'a> Display for Expr<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Expr::Prim((_, prim)) => prim.to_string(),
-            Expr::Unary((_, una)) => una.to_string(),
-            Expr::Binary((_, bin)) => bin.to_string(),
+            Expr::Prim(expr) => write!(f, "{}", expr.1),
+            Expr::Unary(expr) => write!(f, "{}", expr.1),
+            Expr::Binary(expr) => write!(f, "{}", expr.1),
         }
     }
 }
@@ -176,19 +405,19 @@ pub enum PrimExpr<'a> {
     Call(Sourced<&'a str>, Sourced<FuncArgs<'a>>),
 }
 
-impl<'a> ToString for PrimExpr<'a> {
-    fn to_string(&self) -> String {
+impl<'a> Display for PrimExpr<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            PrimExpr::LValue((_, lval)) => lval.to_string(),
-            PrimExpr::Literal((_, literal)) => literal.to_string(),
-            PrimExpr::Paren(expr) => expr.1.to_string(),
-            PrimExpr::Call(func, sourced_args) => {
-                let arg = sourced_args
+            PrimExpr::LValue(expr) => write!(f, "{}", expr.1),
+            PrimExpr::Literal(expr) => write!(f, "{}", expr.1),
+            PrimExpr::Paren(expr) => write!(f, "({})", expr.1),
+            PrimExpr::Call(func, args) => {
+                let args = args
                     .1
                     .iter()
                     .map(|arg| arg.1.to_string())
                     .collect::<Vec<String>>();
-                format!("{}({})", func.1, arg.join(", "))
+                write!(f, "{}({})", func.1, args.join(", "))
             }
         }
     }
@@ -205,13 +434,13 @@ pub enum UnaryOp {
     // Cast(AstType),
 }
 
-impl UnaryOp {
-    pub fn to_str(&self) -> &'static str {
+impl Display for UnaryOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            UnaryOp::Pos => "+",
-            UnaryOp::Neg => "-",
-            UnaryOp::Not => "!",
-            UnaryOp::BitNot => "~",
+            UnaryOp::Pos => write!(f, "+"),
+            UnaryOp::Neg => write!(f, "-"),
+            UnaryOp::Not => write!(f, "!"),
+            UnaryOp::BitNot => write!(f, "~"),
         }
     }
 }
@@ -222,9 +451,9 @@ pub struct Unary<'a> {
     pub rhs: Box<Sourced<Expr<'a>>>,
 }
 
-impl<'a> ToString for Unary<'a> {
-    fn to_string(&self) -> String {
-        format!("({}{})", self.op.1.to_str(), self.rhs.1.to_string())
+impl<'a> Display for Unary<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}{})", self.op.1, self.rhs.1)
     }
 }
 
@@ -250,27 +479,27 @@ pub enum BinaryOp {
     Mod,
 }
 
-impl BinaryOp {
-    pub fn to_str(&self) -> &'static str {
+impl Display for BinaryOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            BinaryOp::LogicalOr => "||",
-            BinaryOp::LogicalAnd => "&&",
-            BinaryOp::BitOr => "|",
-            BinaryOp::BitXor => "^",
-            BinaryOp::BitAnd => "&",
-            BinaryOp::Eq => "==",
-            BinaryOp::Ne => "!=",
-            BinaryOp::Lt => "<",
-            BinaryOp::Gt => ">",
-            BinaryOp::Le => "<=",
-            BinaryOp::Ge => ">=",
-            BinaryOp::Shl => "<<",
-            BinaryOp::Shr => ">>",
-            BinaryOp::Add => "+",
-            BinaryOp::Sub => "-",
-            BinaryOp::Mul => "*",
-            BinaryOp::Div => "/",
-            BinaryOp::Mod => "%",
+            BinaryOp::LogicalOr => write!(f, "||"),
+            BinaryOp::LogicalAnd => write!(f, "&&"),
+            BinaryOp::BitOr => write!(f, "|"),
+            BinaryOp::BitXor => write!(f, "^"),
+            BinaryOp::BitAnd => write!(f, "&"),
+            BinaryOp::Eq => write!(f, "=="),
+            BinaryOp::Ne => write!(f, "!="),
+            BinaryOp::Lt => write!(f, "<"),
+            BinaryOp::Gt => write!(f, ">"),
+            BinaryOp::Le => write!(f, "<="),
+            BinaryOp::Ge => write!(f, ">="),
+            BinaryOp::Shl => write!(f, "<<"),
+            BinaryOp::Shr => write!(f, ">>"),
+            BinaryOp::Add => write!(f, "+"),
+            BinaryOp::Sub => write!(f, "-"),
+            BinaryOp::Mul => write!(f, "*"),
+            BinaryOp::Div => write!(f, "/"),
+            BinaryOp::Mod => write!(f, "%"),
         }
     }
 }
@@ -282,14 +511,9 @@ pub struct Binary<'a> {
     pub rhs: Box<Sourced<Expr<'a>>>,
 }
 
-impl<'a> ToString for Binary<'a> {
-    fn to_string(&self) -> String {
-        format!(
-            "({} {} {})",
-            self.lhs.1.to_string(),
-            self.op.1.to_str(),
-            self.rhs.1.to_string()
-        )
+impl<'a> Display for Binary<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} {} {})", self.lhs.1, self.op.1, self.rhs.1)
     }
 }
 
@@ -297,18 +521,19 @@ impl<'a> ToString for Binary<'a> {
 pub enum LValue<'a> {
     Ident(Sourced<&'a str>),
     Idx(Sourced<&'a str>, Vec<Sourced<Expr<'a>>>),
-    // Deref(Box<AstExpr>),
-    // Paren(Box<AstLValue>),
 }
 
-impl<'a> ToString for LValue<'a> {
-    fn to_string(&self) -> String {
+impl<'a> Display for LValue<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            LValue::Ident(ident) => ident.1.to_string(),
-            LValue::Idx(arr, idxs) => idxs.iter().fold(arr.1.to_string(), |mut acc, idx| {
-                acc.push_str(&format!("[{}]", idx.1.to_string()));
-                acc
-            }),
+            LValue::Ident(ident) => write!(f, "{}", ident.1),
+            LValue::Idx(ident, idx) => {
+                let idx = idx.iter().fold("".to_owned(), |mut acc, item| {
+                    acc.push_str(&format!("[{}]", item.1));
+                    acc
+                });
+                write!(f, "{}{}", ident.1, idx)
+            }
         }
     }
 }
--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,17 +1,72 @@
 pub mod error;
-pub mod parser;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use lalrpop_util::lalrpop_mod;
+lalrpop_mod!(pub syntax); // synthesized by LALRPOP
+pub use syntax::*;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
+    use super::ModuleParser;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn expr0() {
+        let code = r##"
+int g;
+int h;
+int f;
+int e;
+int eightwhile() {
+  int a;
+  a = 5;
+  int b;
+  int c;
+  b = 6;
+  c = 7;
+  int d;
+  d = 10;
+  while (a < 20) {
+    a = a + 3;
+    while (b < 10) {
+      b = b + 1;
+      while (c == 7) {
+        c = c - 1;
+        while (d < 20) {
+          d = d + 3;
+          while (e > 1) {
+            e = e - 1;
+            while (f > 2) {
+              f = f - 2;
+              while (g < 3) {
+                g = g + 10;
+                while (h < 10) {
+                  h = h + 8;
+                }
+                h = h - 1;
+              }
+              g = g - 8;
+            }
+            f = f + 1;
+          }
+          e = e + 1;
+        }
+        d = d - 1;
+      }
+      c = c + 1;
+    }
+    b = b - 2;
+  }
+
+  return (a + (b + d) + c) - (e + d - g + h);
+}
+
+int main() {
+  g = 1;
+  h = 2;
+  e = 4;
+  f = 6;
+  return eightwhile();
+}
+            "##;
+        let parsed = ModuleParser::new().parse(code).unwrap();
+        println!("{parsed}");
     }
 }
